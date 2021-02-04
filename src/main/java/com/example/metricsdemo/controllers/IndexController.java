b53d5ec9dcbafaeef3abc4afc590625a46ab5de4
Richard Walker <rwalker@matebook.localdomain>
2021-02-04 12:12:16 +0000

    first commit

---
+++ src/main/java/com/example/metricsdemo/controllers/IndexController.java
@@ -0,0 +1,26 @@
+package com.example.metricsdemo.controllers;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class IndexController {
+
+    @Autowired
+    private MeterRegistry meterRegistry;
+
+    private String message = "Custom Metric Demo";
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        // Custom Metric
+        meterRegistry.counter("custom_requests_total").increment();
+
+        model.addAttribute("greeting", message );
+        model.addAttribute("counter",  meterRegistry.counter("custom_requests_total").count() );
+        return "index";
+    }
+}
