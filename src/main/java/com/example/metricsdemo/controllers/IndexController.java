93d03f14e4eef851857a7518b59779c74577f7db
Richard Walker <rwalker@matebook.localdomain>
2021-02-09 12:28:44 +0000

    added more metrics

--- src/main/java/com/example/metricsdemo/controllers/IndexController.java
+++ src/main/java/com/example/metricsdemo/controllers/IndexController.java
@@ -10,17 +10,27 @@ import org.springframework.ui.Model;
 public class IndexController {
 
     @Autowired
-    private MeterRegistry meterRegistry;
+    private MeterRegistry meterRegistry1;
+
+    @Autowired
+    private MeterRegistry meterRegistry2;
+
+    @Autowired
+    private MeterRegistry meterRegistry3;
 
     private String message = "Custom Metric Demo";
 
     @RequestMapping("/")
     public String index(Model model) {
         // Custom Metric
-        meterRegistry.counter("custom_requests_total").increment();
+        meterRegistry1.counter("custom_requests_total").increment();
+        meterRegistry2.counter("custom_sales_total").increment(2);
+        meterRegistry3.counter("custom_balance_total").increment(10);
 
         model.addAttribute("greeting", message );
-        model.addAttribute("counter",  meterRegistry.counter("custom_requests_total").count() );
+        model.addAttribute("counter",  meterRegistry1.counter("custom_requests_total").count() );
+        model.addAttribute("sales",  meterRegistry2.counter("custom_sales_total").count() );
+        model.addAttribute("balance",  meterRegistry2.counter("custom_balance_total").count() );
         return "index";
     }
 }
